f53beaf937025e65e3b35e072161dd5591a5961b
Date <tjjh89017@hotmail.com>
2013-12-17 18:00:51 +0800

    update

--- hw3/main.c
+++ hw3/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 #include "mybtree.h"
 
@@ -18,34 +20,50 @@ typedef struct{
 int compare(void *a, void *b);
 mybtree* huffman_tree(frequency **f, int len, int (*compare_fun)(void*, void*));
 char* huffman_encode(mybtree *huff, char *s, int len);
-
-void re(mybtree *huff)
-{
-	if(huff->left == NULL && huff->right == NULL){
-		printf("%c\n", *(char*)((frequency*)huff->value)->symbol);
-		return;
-	}
-	re(huff->left);
-	re(huff->right);
-}
+int search_symbol(mybtree *huff, char c, char *s);
 
 int main(){
 
-	printf("input how many symbol\n");
+	char s[1000];
 	int len = 0;
+	printf("input how many symbol\n");
 	scanf("%d", &len);
 	char *sym = (char*)malloc((len + 1) * sizeof(char));
 	frequency **f = (frequency**)malloc(len * sizeof(frequency*));
 
-	int i = 0;
+	int i = 0, j = 0;
 	for(i = 0; i < len; i++){
 		f[i] = (frequency*)malloc(sizeof(frequency));
 		scanf("%s %d", &sym[i], &f[i]->weight);
 		f[i]->symbol = &sym[i];
 	}
+	printf("input the string need to encode\n");
+	scanf("%s", s);
+	int slen = strlen(s);
 
 	mybtree *huffman = huffman_tree(f, len, compare);
-	re(huffman);
+	
+	char **table = (char**)malloc(len * sizeof(char*));
+	int l = log2(len) + 2;
+	for(i = 0; i < len; i++){
+		table[i] = (char*)malloc(l * sizeof(char));
+		search_symbol(huffman, sym[i], table[i]);
+		printf("%c: %s\n", sym[i], table[i]);
+	}
+
+	char *encode = (char*)malloc(l * slen * sizeof(char));
+	char *ptr = encode;
+	for(i = 0; i < slen; i++){
+		// search symbol
+		for(j = 0; j < len; j++){
+			if(s[i] == sym[j]){
+				ptr += sprintf(ptr, table[j]);
+				break;
+			}
+		}
+	}
+
+	printf("%s: %s\n", s, encode);
 	return 0;
 }
 
@@ -58,7 +76,7 @@ mybtree* huffman_tree(frequency **f, int len, int (*compare_fun)(void*, void*))
 {
 	// let f be a min heap
 	int i = 0;
-	mybtree **heap = (mybtree*)malloc(len * sizeof(mybtree*));
+	mybtree **heap = (mybtree**)malloc(len * sizeof(mybtree*));
 	for(i = 0; i < len; i++)
 		heap[i] = mybtree_new(f[i]);
 
@@ -100,9 +118,9 @@ mybtree* huffman_tree(frequency **f, int len, int (*compare_fun)(void*, void*))
 			parent = 0;
 			son = parent * 2 + 1;
 			while(son < len){
-				if(son < len - 1 && compare_fun(heap[son]->value, heap[son + 1]->value) < 0)
+				if(son < len - 1 && compare_fun(heap[son]->value, heap[son + 1]->value) > 0)
 					son++;
-				if(compare_fun(heap[parent]->value, heap[parent]->value) < 0)
+				if(compare_fun(heap[parent]->value, heap[son]->value) < 0)
 					break;
 				SWAP(heap[parent], heap[son], temp);
 				parent = son;
@@ -124,4 +142,26 @@ mybtree* huffman_tree(frequency **f, int len, int (*compare_fun)(void*, void*))
 	free(heap);
 
 	return root;
+}
+
+int search_symbol(mybtree *huff, char c, char *s)
+{
+	if(huff == NULL)
+		return 0;
+
+	frequency *f = huff->value;
+	if(f->symbol == NULL){
+		if(search_symbol(huff->left, c, s + 1))
+			*s = '0';
+		else if(search_symbol(huff->right, c, s + 1))
+			*s = '1';
+		else
+			return 0;
+
+		return 1;
+	}
+	
+	char val = *(char*)f->symbol;
+	*s = '\0';
+	return (val == c);
 }
