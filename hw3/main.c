66c26b19795fd30cc5b39f63008e5222bb372165
Date <tjjh89017@hotmail.com>
2013-12-17 14:35:48 +0800

    update

---
+++ hw3/main.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mybtree.h"
+
+#define SWAP(a, b, temp) \
+{ \
+	temp = a; \
+	a = b; \
+	b = temp; \
+}
+
+typedef struct{
+	void *symbol;
+	int weight;
+}frequency;
+
+int compare(void *a, void *b);
+mybtree* huffman_tree(frequency **f, int len, int (*compare_fun)(void*, void*));
+
+int main(){
+
+	printf("input how many symbol\n");
+	int len = 0;
+	scanf("%d", &len);
+	char *sym = (char*)malloc((len + 1) * sizeof(char));
+	frequency **f = (frequency**)malloc(len * sizeof(frequency*));
+
+	int i = 0;
+	for(i = 0; i < len; i++){
+		f[i] = (frequency*)malloc(sizeof(frequency));
+		scanf("%s %d", &sym[i], &f[i]->weight);
+		f[i]->symbol = &sym[i];
+	}
+
+	mybtree *huffman = huffman_tree(f, len, compare);
+
+	return 0;
+}
+
+int compare(void *a, void *b)
+{
+	return (((frequency*)a)->weight - ((frequency*)b)->weight);
+}
+
+mybtree* huffman_tree(frequency **f, int len, int (*compare_fun)(void*, void*))
+{
+	// let f be a min heap
+	int i = 0;
+	mybtree **heap = (mybtree*)malloc(len * sizeof(mybtree*));
+	for(i = 0; i < len; i++)
+		heap[i] = mybtree_new(f[i]);
+
+	int son = 0;
+	int parent = 0;
+	mybtree *temp;
+	for(i = 0; i < len; i++){
+		son = i;
+		parent = (son - 1) / 2;
+		while(parent >= 0 && compare_fun(heap[son]->value, heap[parent]->value) < 0){
+			SWAP(heap[son], heap[parent], temp);
+			son = parent;
+			parent = (son - 1) / 2;
+		}
+	}
+
+	// get min 2 value and create a tree
+	// left 0, right 1, and right smaller than left
+	mybtree *root;
+	frequency *fre;
+	son = 1;
+	parent = 0;
+	while(len > 1){
+		fre = (frequency*)malloc(sizeof(frequency));
+		fre->symbol = NULL;
+		fre->weight = 0;
+		root = mybtree_new(fre);
+
+		for(i = 0; i < 2; i++){
+			SWAP(heap[0], heap[len - 1], temp);
+			len--;
+			if(i == 0)
+				root->right = heap[len];
+			else
+				root->left = heap[len];
+			fre->weight += ((frequency*)heap[len]->value)->weight;
+
+			// re-heap
+			parent = 0;
+			son = parent * 2 + 1;
+			while(son < len){
+				if(son < len - 1 && compare_fun(heap[son]->value, heap[son + 1]->value) < 0)
+					son++;
+				if(compare_fun(heap[parent]->value, heap[parent]->value) < 0)
+					break;
+				SWAP(heap[parent], heap[son], temp);
+				parent = son;
+				son = parent * 2 + 1;
+			}
+
+		}
+		heap[len] = root;
+		len++;
+	}
+	free(heap);
+
+	return root;
+}
